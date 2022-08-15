571a0a519417508ca0d3a37584407a1f6aab2dd6
deepuyadav004 <deepuyadavze@gmail.com>
2022-08-15 22:39:54 +0530

    navbar added

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './topbar.css'
+
+function TopBar() {
+  return (
+    <>
+    <div className="top">
+
+        <div className='topLeft'>
+        <i className="topIcon fa-brands fa-facebook"></i>
+        <i className="topIcon fa-brands fa-square-twitter"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
+        <i className="topIcon fa-brands fa-square-pinterest"></i>
+        </div>
+
+
+        <div className='topCenter'>
+            <ul className='topList'>
+                <li className='topListItem'>Home</li>
+                <li className='topListItem'>About</li>
+                <li className='topListItem'>Contact</li>
+                <li className='topListItem'>Write</li>
+                <li className='topListItem'>Logout</li>
+            </ul>
+        </div>
+
+
+        <div className='topRight'>
+            <img className='topImg' src="https://static.remove.bg/remove-bg-web/f9c9a2813e0321c04d66062f8cca92aedbefced7/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png" alt='my'  />
+
+            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default TopBar
