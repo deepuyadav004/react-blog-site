da3cbf43c2e8156afbbeb17aff286f31d1ca3e32
deepuyadav004 <deepuyadavze@gmail.com>
2022-08-20 21:43:27 +0530

    ok

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const dotenv = reqkui
+
+
+// console.log("Hello world")
+
+
+app.listen("5001", () => {
+    console.log("Backend is running.");
+});
